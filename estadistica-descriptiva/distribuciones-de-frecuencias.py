d2296a705fc8612476941f6419d2c360be4178a9
91-julian-sanchez <91.julian.sanchez@gmail.com>
2020-09-10 02:22:46 -0500

    Grafica de pie

--- estadistica-descriptiva/distribuciones-de-frecuencias.py
+++ estadistica-descriptiva/distribuciones-de-frecuencias.py
@@ -1,23 +1,51 @@
 import pandas as pd
 
-def graphic(x_values,y_values,title=None):
-    from bokeh.io import output_file, show
-    from bokeh.plotting import figure
+def graphic(figure=None,x_values=None,y_values=None,title=None,data=None):
 
-    output_file("bar_basic.html")
+    if figure is 'bar_basic':
 
-    p = figure(x_range=x_values, plot_height=350, title=title,
-               toolbar_location=None, tools="")
+        from bokeh.io import output_file, show
+        from bokeh.plotting import figure
 
-    p.vbar(x=x_values, top=y_values, width=0.9)
+        output_file("graphics/bar_basic.html")
 
-    p.xgrid.grid_line_color = None
-    p.y_range.start = 0
+        p = figure(x_range=x_values, plot_height=350, title=title,
+                   toolbar_location=None, tools="")
 
-    show(p)
+        p.vbar(x=x_values, top=y_values, width=0.9)
+
+        p.xgrid.grid_line_color = None
+        p.y_range.start = 0
+
+        show(p)
+
+    elif figure is 'pie':
+        from math import pi
+        from bokeh.io import output_file, show
+        from bokeh.palettes import Category20c
+        from bokeh.plotting import figure
+        from bokeh.transform import cumsum
+
+        output_file("graphics/pie.html")
+
+        data['angle'] = data['value'] / data['value'].sum() * 2 * pi
+        data['color'] = Category20c[len(data)]
+
+        p = figure(plot_height=350, title=title, toolbar_location=None,
+                   tools="hover", tooltips="@class clase: @value")
+
+        p.wedge(x=0, y=1, radius=0.4,
+                start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+                line_color="white", fill_color='color', legend_field='class', source=data)
+
+        p.axis.axis_label = None
+        p.axis.visible = False
+        p.grid.grid_line_color = None
+
+        show(p)
 
 if __name__ == "__main__":
-    print("Hola estadistica descriptiva")
+
     data = pd.read_excel(r'titanic3.xls')
     # print(df)
     df = pd.DataFrame(data)
@@ -27,8 +55,9 @@ if __name__ == "__main__":
 
     print("Frecuencia clases titanic: ")
     frecuencia_de_clases_titanic = df["pclass"].value_counts(ascending=True)
-    print(type(frecuencia_de_clases_titanic))
+    print(frecuencia_de_clases_titanic)
 
+    print("bar:")
     x_values = []
     y_values = []
 
@@ -37,5 +66,12 @@ if __name__ == "__main__":
         x_values.append(f"class {pclass}")
         y_values.append(count)
 
-    graphic(x_values,y_values,"Pasajeros segun su clase en el Titanic")
+    graphic(figure="bar_basic",x_values=x_values,y_values=y_values,title="Pasajeros segun su clase en el Titanic",)
+
+    print("pie:")
+    data = pd.Series(frecuencia_de_clases_titanic).reset_index(name='value').rename(columns={'index': 'class'})
+    print(data)
+    print(data["class"])
+    graphic(figure="pie",title="Pasajeros segun su clase en el Titanic",data=data)
+
     pass
